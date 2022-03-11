39cd5daedaf0d24e71b8015e9528b275483350ed
TVKhanz <91787166+TVKhanz@users.noreply.github.com>
2022-03-11 23:35:32 +0700

    code

--- index.js
+++ index.js
@@ -11,21 +11,21 @@
 // 3. các phép toán:+-*/ | %++ -- ** |
 //  >< , >= , <= , != ,== , === , && , || 
 // BT:
-// let a = parseInt(prompt("nhập vào số a"))
-// let b = parseInt(prompt("nhập vào số b"))
-// let c = parseInt(prompt("nhập vào số c"))
-// let P = a + b + c
-// console.log(`chu vi của tam giác là : ${P}`)
-// let w = parseInt(prompt("nhập vào số cân nặng"))
-// let h = parseFloat(prompt("nhập vào số chiều cao(m)"))
-// let BMI = w /(h**2)
-// console.log( `chỉ sồ BMI của bạn là ${BMI}`);
-// // 4. câu điều kiện
-// if (BMI < 18) {
-//     console.log('bạn hơi gầy');
-// }else if ( BMI >=18 && BMI < 25){
-//     console.log("bạn cân đối");
-// }
-// else{
-//     console.log(" bạn béo");
-// }
+let a = parseInt(prompt("nhập vào số a"))
+let b = parseInt(prompt("nhập vào số b"))
+let c = parseInt(prompt("nhập vào số c"))
+let P = a + b + c
+console.log(`chu vi của tam giác là : ${P}`)
+let w = parseInt(prompt("nhập vào số cân nặng"))
+let h = parseFloat(prompt("nhập vào số chiều cao(m)"))
+let BMI = w /(h**2)
+console.log( `chỉ sồ BMI của bạn là ${BMI}`);
+// 4. câu điều kiện
+if (BMI < 18) {
+    console.log('bạn hơi gầy');
+}else if ( BMI >=18 && BMI < 25){
+    console.log("bạn cân đối");
+}
+else{
+    console.log(" bạn béo");
+}
